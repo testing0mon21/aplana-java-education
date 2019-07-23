cad149491a2431fc52cd76630c591087555bcb29
Kuznetsov <Kuznetsov.S.D@sberbank.ru>
2019-07-23 15:00:42 +0300

    Задание 4

--- src/task4.java
+++ src/task4.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
+// Ввести с консоли число в бинарном формате. Перевести его в десятичный формат, записать в переменную int и вывести на экран.
+
 public class task4 {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Число в бинарном формате: ");
+        String bin = scanner.nextLine();
+        int bin = Integer.parseInt(bin, 2);
+        System.out.println(bin);
+    }
 }
